240f92da78550d208dda9002d6dbcffdedb28c57
Oleg Savodyi <s.s4dko@gmail.com>
2023-02-15 00:57:47 +0200

    Change color error message

--- src/components/atoms/InputField/index.js
+++ src/components/atoms/InputField/index.js
@@ -1,21 +1,26 @@
 import React from 'react';
 import TextField from '@mui/material/TextField';
 
+import '@atoms/InputField/index.module.css'
+import Typography from "@mui/material/Typography";
+
 function InputField({
   label, name, input, meta: { touched, error }, ...other
 }) {
+  const isError = touched && error;
   return (
     <>
       <TextField
         id={name}
         label={label}
         variant="standard"
+        error={isError}
         {...input}
         {...other}
       />
 
-      { touched && error &&
-          <div className={'errorLabel'}>{error}</div>
+      { isError &&
+          <Typography className="errorLabel">{error}</Typography>
       }
     </>
   );
